b21b476c30b6f4eb55170acf537baf79fc6ca5f2
Ramon Hagenaars <ramonhagenaars@gmail.com>
2015-01-19 20:57:00 +0100

    Added the Rook and a RookTest

---
+++ src/nl/axians/chess/movement/RookMove.scala
@@ -0,0 +1,13 @@
+package nl.axians.chess.movement
+
+import nl.axians.chess.pieces.Pawn
+import nl.axians.chess.Location
+import nl.axians.chess.game.Board
+import Math.min
+import nl.axians.chess.pieces.Rook
+
+class RookMove(rook: Rook, from: Location, to: Location) extends RegularMove(from, to) {
+
+  override def validate(b: Board) =
+    deltaX != deltaY && min(deltaX, deltaY) == 0 && b.getOccupiedLocationsBetween(from, to) == Nil
+}
---
+++ testing/nl/axians/chess/unit/pieces/RookTest.scala
@@ -0,0 +1,30 @@
+package nl.axians.chess.unit.pieces
+
+import org.scalatest.FlatSpec
+import nl.axians.chess.game.DefaultBoard
+import nl.axians.chess.movement.PawnMove
+import nl.axians.chess.pieces.Pawn
+import nl.axians.chess.Color._
+import nl.axians.chess.Location
+import nl.axians.chess.movement.KnightMove
+import nl.axians.chess.pieces.Knight
+import nl.axians.chess.movement.RookMove
+import nl.axians.chess.pieces.Rook
+import nl.axians.chess.game.Board
+
+class RookTest extends FlatSpec {
+
+  "A rook" should "be able to move a straight lines" in {
+    var b: Board = new DefaultBoard
+    
+    // A pawn is in the way.
+    assert(!new RookMove(new Rook(WHITE), Location('A', 1), Location('A', 3)).isValid(b))
+    // Move the pawn far away to make room for the rook.
+    b = b.movePiece(Location('A', 2), Location('H', 3))
+    // The move should now be valid
+    assert(new RookMove(new Rook(WHITE), Location('A', 1), Location('A', 3)).isValid(b))
+    b = b.movePiece(Location('A', 1), Location('A', 3))
+    // Non-straight lines are invalid.
+    assert(!new RookMove(new Rook(WHITE), Location('A', 3), Location('H', 4)).isValid(b))
+  }
+}
